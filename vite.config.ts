28dc29ded8b446147faf576bf96e13bd1f7ef2ad
Lukas Raila <lukas.raila@zenitech.co.uk>
2024-10-22 23:41:01 +0300

    Placeholder UI components

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import path from 'path';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -21,4 +22,9 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'app'),
+    },
+  },
 });
