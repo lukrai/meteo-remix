2af1735694b57b37eb750a26a708ea0fe9c42088
Lukas Raila <lukas.raila@zenitech.co.uk>
2024-10-23 09:50:47 +0300

    Testing icons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Card, CardHeader, CardDescription, CardTitle, CardContent, CardFooter } from "~/components/ui/card";
 import { Toggle } from "~/components/ui/toggle"
+import { Icons } from "~/components/ui/icons";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,27 +22,23 @@ export default function Index() {
         </div>
 
         <div className="grid w-full gap-6 sm:grid-cols-1 lg:max-w-[42rem] md:grid-cols-2 xl:max-w-[42rem]">
-          <Card
-            className="lg:max-w-md" x-chunk="charts-01-chunk-0"
-          >
+          <Card className="lg:max-w-md">
             <CardHeader className="space-y-0 pb-2">
-              <CardDescription>Today</CardDescription>
-              <CardTitle className="text-4xl tabular-nums">
-                12,584{" "}
-                <span className="font-sans text-sm font-normal tracking-normal text-muted-foreground">
-                  steps
-                </span>
-              </CardTitle>
+              <CardDescription className="text-4xl">Vilnius</CardDescription>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <Icons.sun className="h-64 w-64" />
+              <div className="grid w-full gap-6 sm:grid-cols-2">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
           </Card>
           <Card
-            className="lg:max-w-md" x-chunk="charts-01-chunk-0"
+            className="lg:max-w-md"
           >
             <CardHeader className="space-y-0 pb-2">
               <CardDescription>Today</CardDescription>
@@ -60,10 +57,11 @@ export default function Index() {
             </CardFooter>
           </Card>
         </div>
-        <div className="flex gap-4">
-          Place Holder
-        </div>
-
+        <Card className="flex gap-4 w-full">
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
